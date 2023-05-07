06431816e8f69bb8f93cbdc3b1fc051798190eb4
“Enter <jadhavmahesh529@gmail.com>
2023-05-08 00:43:40 +0530

    Add:Dictionary

--- Compute5.sh
+++ Compute5.sh
@@ -1,8 +1,6 @@
+declare -A results
+
+result1=$((a + b * c))
+result2=$((a * b + c * partTimeHour))
 result3=$((c + a / b))
-
-result4=$((a % b + c))
-
-echo "result1: $result1"
-echo "result2: $result2"
-echo "result3: $result3"
-echo "result4: $result4"
+result4=$((a % b + c)
