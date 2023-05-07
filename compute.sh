1dee26583e96b9e1250293b4d6a8b7cf59c232ac
“Enter <jadhavmahesh529@gmail.com>
2023-05-08 00:01:40 +0530

    Add:Compute a + b * c

---
+++ compute.sh
@@ -0,0 +1,11 @@
+read -p "Enter value for a:" num
+read -p "Enter value for a:" a
+
+read -p "Enter value for b:" num
+read -p "Enter value for b:" b
+
+read -p "Enter value for c:" c
+
+result=$((a + b * c))
+
+read -p "Enter value for c:" num
