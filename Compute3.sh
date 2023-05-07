8c7b86bf7898cf8d0a623337c30d475a699dc281
“Enter <jadhavmahesh529@gmail.com>
2023-05-08 00:15:19 +0530

    Add:Compute a * b + c - Assume Part time Hour is 8

---
+++ Compute3.sh
@@ -0,0 +1,11 @@
+read -p "Enter value for a:" a
+
+read -p "Enter value for b:" b
+
+read -p "Enter value for c:" c
+
+result=$((a + b * c))
+result1=$((a + b * c))
+
+partTimeHour=8
+result2=$((a * b + c * partTimeHour))
